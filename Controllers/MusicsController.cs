5030018ad3d1d4f16016967f490997c66ec6a38f
codypronnier76130 <60094357+codypronnier76130@users.noreply.github.com>
2021-02-12 15:39:54 +0100

    Add files via upload

---
+++ Controllers/MusicsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMusic.Data;
+using MvcMusic.Models;
+
+namespace MvcMusic.Controllers
+{
+    public class MusicsController : Controller
+    {
+        private readonly MvcMusicContext _context;
+
+        public MusicsController(MvcMusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Musics
+        public async Task<IActionResult> Index(string musicGenre, string searchString)
+        {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Music
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var musics = from m in _context.Music
+                 select m;
+
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            musics = musics.Where(s => s.Title.Contains(searchString));
+        }
+         if (!string.IsNullOrEmpty(musicGenre))
+        {
+        musics = musics.Where(x => x.Genre == musicGenre);
+        }
+        var musicGenreVM = new MusicGenreViewModel
+        {
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+            Music = await musics.ToListAsync()
+        };
+
+            return View(musicGenreVM);
+        }
+
+        // GET: Musics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Music
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // GET: Musics/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Musics/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Music music)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(music);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Music.FindAsync(id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            return View(music);
+        }
+
+        // POST: Musics/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Music music)
+        {
+            if (id != music.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(music);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MusicExists(music.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Music
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // POST: Movies/Delete/6
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, bool notUsed)
+        {
+            var music = await _context.Music.FindAsync(id);
+            _context.Music.Remove(music);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MusicExists(int id)
+        {
+            return _context.Music.Any(e => e.Id == id);
+        }
+    }
+}
